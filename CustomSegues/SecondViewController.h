460e1b4d63da6a333bac17c63b25ed683917d6cf
Mikael <miketekle14@gmail.com>
2016-10-23 13:35:08 -0700

    created header and implementation subclass files for second view controllers

---
+++ CustomSegues/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  CustomSegues
+//
+//  Created by Mikael Teklehaimanot on 10/23/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
